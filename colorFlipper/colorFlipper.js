703c2527ab27ce313b642245c2754dafc9e02617
Arham Shajahan <universalmadman@gmail.com>
2024-07-03 10:07:23 +0530

    Little Changes

--- colorFlipper/colorFlipper.js
+++ colorFlipper/colorFlipper.js
@@ -4,11 +4,13 @@ const colors = ['#5DADE2','#48C9B0','#F1948A','#BB8FCE','#BB8FCE',
 const button = document.getElementById('btn');
 const color = document.querySelector('.color-1');
 
+
 button.addEventListener("click",
     ()=>{
         // here we use the random numbers to pick the random hex code.
+        
         const randomNumber = Math.floor((Math.random() * colors.length))
         document.body.style.backgroundColor = colors[randomNumber];
         color.textContent = colors[randomNumber];
-    }
+       }
 )
